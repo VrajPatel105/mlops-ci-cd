aea785aedffd0de4d9cb44e431596a5320a687c4
VrajPatel105 <vrajp1052005@gmail.com>
2026-09-14 15:07:40 -0400

    bug fixes for docker final image push

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,8 @@ COPY models/vectorizer.pkl /docker_app/models/vectorizer.pkl
 
 RUN pip install --no-cache-dir -r /docker_app/requirements.txt
 
+RUN python -m nltk.downloader stopwords wordnet
+
 EXPOSE 5000
 
 CMD ["python", "/docker_app/main.py"]
--- app/main.py
+++ app/main.py
@@ -1,10 +1,9 @@
-# updated app.py
-
 from flask import Flask, render_template, request
 import mlflow
 import pickle
 import os
 import pandas as pd
+
 
 import numpy as np
 import pandas as pd
@@ -16,6 +15,7 @@ from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 import dagshub
 
+
 def lemmatization(text):
     """Lemmatize the text."""
     lemmatizer = WordNetLemmatizer()
@@ -23,22 +23,26 @@ def lemmatization(text):
     text = [lemmatizer.lemmatize(word) for word in text]
     return " ".join(text)
 
+
 def remove_stop_words(text):
     """Remove stop words from the text."""
     stop_words = set(stopwords.words("english"))
     text = [word for word in str(text).split() if word not in stop_words]
     return " ".join(text)
 
+
 def removing_numbers(text):
     """Remove numbers from the text."""
     text = ''.join([char for char in text if not char.isdigit()])
     return text
+
 
 def lower_case(text):
     """Convert text to lower case."""
     text = text.split()
     text = [word.lower() for word in text]
     return " ".join(text)
+
 
 def removing_punctuations(text):
     """Remove punctuations from the text."""
@@ -47,16 +51,19 @@ def removing_punctuations(text):
     text = re.sub('\\s+', ' ', text).strip()
     return text
 
+
 def removing_urls(text):
     """Remove URLs from the text."""
     url_pattern = re.compile(r'https?://\S+|www\.\S+')
     return url_pattern.sub(r'', text)
+
 
 def remove_small_sentences(df):
     """Remove sentences with less than 3 words."""
     for i in range(len(df)):
         if len(df.text.iloc[i].split()) < 3:
             df.text.iloc[i] = np.nan
+
 
 def normalize_text(text):
     text = lower_case(text)
@@ -68,21 +75,27 @@ def normalize_text(text):
 
     return text
 
+
 # Set up DagsHub credentials for MLflow tracking
 dagshub_token = os.getenv("DAGSHUB_PAT")
 if not dagshub_token:
     raise EnvironmentError("DAGSHUB_PAT environment variable is not set")
 
+
 os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
 os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+
 
 dagshub_url = "https://dagshub.com"
 repo_owner = "VrajPatel105"
 repo_name = "mlops-ci-cd"
 
+
 mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 
+
 app = Flask(__name__)
+
 
 # load model from model registry
 def get_latest_model_version(model_name):
@@ -92,41 +105,50 @@ def get_latest_model_version(model_name):
         latest_version = client.get_latest_versions(model_name, stages=["None"])
     return latest_version[0].version if latest_version else None
 
+
 model_name = "my_model"
 model_version = get_latest_model_version(model_name)
+
 
 model_uri = f'models:/{model_name}/{model_version}'
 model = mlflow.pyfunc.load_model(model_uri)
 
-vectorizer = pickle.load(open('models/vectorizer.pkl','rb'))
 
-with open("models/model.pkl", "rb") as f:
-    clf = pickle.load(f)
+with open("/docker_app/models/vectorizer.pkl", "rb") as f:
+    vectorizer = pickle.load(f)
 
-print("Model expects:", clf.n_features_in_)
+
 print("Vectorizer produces:", len(vectorizer.get_feature_names_out()))
+
 
 @app.route("/")
 def home():
     return render_template("index.html", result=None)
 
 
+
 @app.route("/predict", methods=["POST"])
 def predict():
     text = request.form["text"]
 
+
     text = normalize_text(text)
 
+
     features = vectorizer.transform([text])
+
 
     features_df = pd.DataFrame(
         features.toarray(),
         columns=[str(i) for i in range(features.shape[1])]
     )
 
+
     result = model.predict(features_df)
 
+
     return render_template("index.html", result=result[0])
+
 
 
 if __name__ == "__main__":
